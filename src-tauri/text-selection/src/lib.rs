1cedc7e9d8241a98bf65cdcc4b8762e0244d8162
Jerry <x.jerry.wang@gmail.com>
2025-02-24 09:51:21 +0000

    feat: check double click

--- src-tauri/text-selection/src/lib.rs
+++ src-tauri/text-selection/src/lib.rs
@@ -20,8 +20,9 @@ pub use types::*;
 #[derive(Debug)]
 struct SelectionEventMark {
     mouse_down_pos: (f64, f64),
-    mouse_down: bool,
     mouse_down_ts: Instant,
+    last_click_ts: Option<Instant>,
+    last_click_pos: (f64, f64),
 }
 
 #[derive(Debug)]
@@ -38,62 +39,77 @@ pub fn listen<T: Fn(ListenResult) -> () + 'static>(listener: T) -> result::Resul
 
     let mut event_marker = SelectionEventMark {
         mouse_down_pos: (0.0, 0.0),
-        mouse_down: false,
         mouse_down_ts: Instant::now(),
+        last_click_ts: None,
+        last_click_pos: (0.0, 0.0),
     };
 
     //
-    rdev::listen(move |event| {
-        match event.event_type {
-            EventType::ButtonPress(Button::Left) => {
-                event_marker.mouse_down_pos = host.get_mouse_position();
-                event_marker.mouse_down = true;
-                event_marker.mouse_down_ts = Instant::now();
-            }
-
-            EventType::ButtonRelease(Button::Left) => {
-                let mut should_check_selection = false;
-
-                let current_mouse_pos = host.get_mouse_position();
-
-                if event_marker.mouse_down {
-                    let now = Instant::now();
-
-                    let is_passed_distance_check =
-                        utils::distance(event_marker.mouse_down_pos, current_mouse_pos) > 5.0;
-
-                    if now.duration_since(event_marker.mouse_down_ts) > Duration::from_millis(50)
-                        && is_passed_distance_check
-                    {
-                        should_check_selection = true;
-                    }
-                }
-
-                if should_check_selection {
-                    match detect_selected_text(&host) {
-                        Ok(val) => match val {
-                            TextSelectionDetectResult::Selected => {
-                                println!("detected selected text")
-                            }
-                            TextSelectionDetectResult::Text(s) => {
-                                // println!("mouse position {:?}", current_mouse_pos);
-
-                                println!("selected text: {}", s)
-                            }
-                            TextSelectionDetectResult::None => {
-                                println!("no selected text")
-                            }
-                        },
-                        Err(err) => {
-                            println!("err {:?}", err);
-                        }
-                    }
-                }
-
-                event_marker.mouse_down = false;
-            }
-            _ => {}
+    rdev::listen(move |event| match event.event_type {
+        EventType::ButtonPress(Button::Left) => {
+            event_marker.mouse_down_pos = host.get_mouse_position();
+            event_marker.mouse_down_ts = Instant::now();
         }
+
+        EventType::ButtonRelease(Button::Left) => {
+            let mut should_check_selection = false;
+
+            let current_mouse_pos = host.get_mouse_position();
+
+            let maybe_click = utils::distance(event_marker.mouse_down_pos, current_mouse_pos) < 5.0;
+
+            if maybe_click {
+                let now = Instant::now();
+
+                if let Some(last_click_ts) = event_marker.last_click_ts {
+                    let db_click_max_delay_check_ms = 500;
+
+                    let maybe_double_click = now.duration_since(last_click_ts)
+                        < Duration::from_millis(db_click_max_delay_check_ms)
+                        && utils::distance(event_marker.last_click_pos, current_mouse_pos) < 5.0;
+
+                    if maybe_double_click {
+                        should_check_selection = true;
+                        event_marker.last_click_ts = None;
+                    } else {
+                        event_marker.last_click_ts = Some(now);
+                        event_marker.last_click_pos = current_mouse_pos.clone();
+                    }
+                } else {
+                    event_marker.last_click_ts = Some(now);
+                    event_marker.last_click_pos = current_mouse_pos.clone();
+                }
+            } else {
+                should_check_selection = true;
+                event_marker.last_click_ts = None;
+            }
+
+            if should_check_selection {
+                match detect_selected_text(&host) {
+                    Ok(val) => match val {
+                        TextSelectionDetectResult::Selected => {
+                            listener(ListenResult {
+                                selected_text: String::default(),
+                                mouse_position: host.get_mouse_position(),
+                            });
+                        }
+                        TextSelectionDetectResult::Text(s) => {
+                            listener(ListenResult {
+                                selected_text: s,
+                                mouse_position: host.get_mouse_position(),
+                            });
+                        }
+                        TextSelectionDetectResult::None => {
+                            println!("no selected text")
+                        }
+                    },
+                    Err(err) => {
+                        println!("err {:?}", err);
+                    }
+                }
+            }
+        }
+        _ => {}
     })
 }
 
