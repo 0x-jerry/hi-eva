4607317504fe5580ffba9cdefbc611587bc19d0f
Jerry <x.jerry.wang@gmail.com>
2025-02-25 01:32:21 +0000

    feat: add text-selection to deps

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,6 +9,14 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            text_selection::listen(|result| {
+
+                println!("selected: {:?}", result);
+            })?;
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
