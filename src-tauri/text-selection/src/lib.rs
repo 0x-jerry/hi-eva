ffd6c27d0311369aa10e95bc9d8880001b37d083
Jerry <x.jerry.wang@gmail.com>
2025-03-13 19:19:13 +0800

    perf(text-selection): optimize selection check frequency

--- src-tauri/text-selection/src/lib.rs
+++ src-tauri/text-selection/src/lib.rs
@@ -18,30 +18,32 @@ use mouce::{
 pub use types::*;
 
 #[derive(Debug)]
-struct SelectionEventMark {
+struct SelectionState {
     mouse_down_pos: (f64, f64),
     mouse_down_ts: Instant,
     last_click_ts: Option<Instant>,
     last_click_pos: (f64, f64),
+    last_check_ts: Instant,
 }
 
 pub fn listen<T: 'static + TextSelectionHandler + Send>(text_selection_handler: T) -> Result<()> {
     #[cfg(target_os = "macos")]
     unix_impl::request_accessibility_access();
 
-    let event_marker = Mutex::new(SelectionEventMark {
+    let state = Mutex::new(SelectionState {
         mouse_down_pos: (0.0, 0.0),
         mouse_down_ts: Instant::now(),
         last_click_ts: None,
         last_click_pos: (0.0, 0.0),
+        last_check_ts: Instant::now(),
     });
 
     let mut mouse = mouce::Mouse::new();
 
     let result = mouse.hook(Box::new(move |event| {
-        let mut event_marker = event_marker.try_lock().unwrap();
+        let mut state = state.try_lock().unwrap();
 
-        handle_mouse_event(event, &mut event_marker, &text_selection_handler);
+        handle_mouse_event(event, &mut state, &text_selection_handler);
     }));
 
     match result {
@@ -52,7 +54,7 @@ pub fn listen<T: 'static + TextSelectionHandler + Send>(text_selection_handler: 
 
 fn handle_mouse_event<T: 'static + TextSelectionHandler + Send>(
     event: &MouseEvent,
-    event_marker: &mut SelectionEventMark,
+    state: &mut SelectionState,
     text_selection_handler: &T,
 ) {
     #[cfg(windows)]
@@ -62,8 +64,8 @@ fn handle_mouse_event<T: 'static + TextSelectionHandler + Send>(
 
     match event {
         MouseEvent::Press(MouseButton::Left) => {
-            event_marker.mouse_down_pos = text_selection_handler.get_cursor_position();
-            event_marker.mouse_down_ts = Instant::now();
+            state.mouse_down_pos = text_selection_handler.get_cursor_position();
+            state.mouse_down_ts = Instant::now();
             text_selection_handler.on_mouse_down();
         }
         MouseEvent::Release(MouseButton::Left) => {
@@ -72,56 +74,61 @@ fn handle_mouse_event<T: 'static + TextSelectionHandler + Send>(
             let current_mouse_pos = text_selection_handler.get_cursor_position();
 
             let now = Instant::now();
-            let maybe_click = utils::distance(event_marker.mouse_down_pos, current_mouse_pos) < 5.0;
+            let maybe_click = utils::distance(state.mouse_down_pos, current_mouse_pos) < 5.0;
 
             if maybe_click {
-                if let Some(last_click_ts) = event_marker.last_click_ts {
+                if let Some(last_click_ts) = state.last_click_ts {
                     let db_click_max_delay_check_ms = 500;
 
                     let maybe_double_click = now.duration_since(last_click_ts)
                         < Duration::from_millis(db_click_max_delay_check_ms)
-                        && utils::distance(event_marker.last_click_pos, current_mouse_pos) < 5.0;
+                        && utils::distance(state.last_click_pos, current_mouse_pos) < 5.0;
 
                     if maybe_double_click {
                         should_check_selection = true;
-                        event_marker.last_click_ts = None;
+                        state.last_click_ts = None;
                     } else {
-                        event_marker.last_click_ts = Some(now);
-                        event_marker.last_click_pos = current_mouse_pos.clone();
+                        state.last_click_ts = Some(now);
+                        state.last_click_pos = current_mouse_pos.clone();
                     }
                 } else {
-                    event_marker.last_click_ts = Some(now);
-                    event_marker.last_click_pos = current_mouse_pos.clone();
+                    state.last_click_ts = Some(now);
+                    state.last_click_pos = current_mouse_pos.clone();
                 }
             } else {
                 should_check_selection = true;
-                event_marker.last_click_ts = None;
+                state.last_click_ts = None;
             }
 
             if should_check_selection {
-                log::info!("check selection start");
+                let should_check_again =
+                    now.duration_since(state.last_check_ts) > Duration::from_millis(100);
 
-                match host.detect_selected_text() {
-                    Ok(val) => match val {
-                        TextSelectionDetectResult::Selected => {
-                            text_selection_handler.on_selection_change(Some(ListenResult {
-                                selected_text: String::default(),
-                            }));
+                if should_check_again {
+                    log::info!("check selection start");
+                    match host.detect_selected_text() {
+                        Ok(val) => match val {
+                            TextSelectionDetectResult::Selected => {
+                                text_selection_handler.on_selection_change(Some(ListenResult {
+                                    selected_text: String::default(),
+                                }));
+                            }
+                            TextSelectionDetectResult::Text(s) => {
+                                text_selection_handler
+                                    .on_selection_change(Some(ListenResult { selected_text: s }));
+                            }
+                            TextSelectionDetectResult::None => {
+                                text_selection_handler.on_selection_change(None);
+                            }
+                        },
+                        Err(err) => {
+                            log::error!("detect selected text error {:?}", err);
                         }
-                        TextSelectionDetectResult::Text(s) => {
-                            text_selection_handler
-                                .on_selection_change(Some(ListenResult { selected_text: s }));
-                        }
-                        TextSelectionDetectResult::None => {
-                            text_selection_handler.on_selection_change(None);
-                        }
-                    },
-                    Err(err) => {
-                        log::error!("detect selected text error {:?}", err);
                     }
-                }
+                    log::info!("check selection end");
 
-                log::info!("check selection end");
+                    state.last_check_ts = now;
+                }
             } else {
                 text_selection_handler.on_selection_change(None);
             }
