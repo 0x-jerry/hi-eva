1963893abd7a69afd0e319e875eb2438879d1260
Jerry <x.jerry.wang@gmail.com>
2025-03-19 15:38:35 +0800

    perf: tweak style

--- src/pages/settings.vue
+++ src/pages/settings.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import { useScroll } from '@vueuse/core'
-import { computed, ref, useId } from 'vue'
+import { onMounted, reactive, ref, useId } from 'vue'
 import AISetting from '../components/settings/AISetting.vue'
 import AboutSetting from '../components/settings/AboutSetting.vue'
 import CommonSetting from '../components/settings/CommonSetting.vue'
 import PromptSetting from '../components/settings/PromptSetting.vue'
+import { useEventListener } from '@vueuse/core'
 
 const scrollElRef = ref<HTMLElement>()
 
@@ -33,31 +33,35 @@ const settings = [
   },
 ].filter((n) => n.visible !== false)
 
-const activeMenu = computed(() => {
-  const top = scroll.y.value
-
-  const hit = settings.find((n) => {
-    const el = document.getElementById(n.id)
-    if (!el) {
-      return false
-    }
-
-    if (top > el.clientTop) {
-      return true
-    }
-  })
-
-  return hit?.id || settings.at(-1)?.id
+const state = reactive({
+  activeMenu: '',
 })
 
-const scroll = useScroll(scrollElRef)
+useEventListener(scrollElRef, 'scroll', (_evt) => {
+  state.activeMenu = getActiveMenuId()
+})
+
+onMounted(() => {
+  state.activeMenu = getActiveMenuId()
+})
+
+function getActiveMenuId() {
+  const hit = settings.find((n) => {
+    const top = document.getElementById(n.id)?.getBoundingClientRect().top ?? -1
+
+    return top >= 0
+  })
+
+  return hit?.id || settings.at(-1)?.id || ''
+}
 
 function clickMenu(conf: { id: string }) {
   const el = document.getElementById(conf.id)
   el?.scrollIntoView({
-    behavior: 'smooth',
     block: 'start',
+    behavior: 'smooth',
   })
+  state.activeMenu = conf.id
 }
 </script>
 <template>
@@ -66,13 +70,13 @@ function clickMenu(conf: { id: string }) {
       <div class="menu-item mb-4 justify-center font-mono bg-light-8 h-8">
         <span>Hi Eva</span>
       </div>
-      <div class="menu-item" v-for="conf in settings" :id="conf.id" @click="clickMenu(conf)"
-        :class="{ active: activeMenu === conf.id }">
+      <div class="menu-item" v-for="conf in settings" :key="conf.id" @click="clickMenu(conf)"
+        :class="{ active: state.activeMenu === conf.id }">
         {{ conf.label }}
       </div>
     </div>
     <div ref="scrollElRef" class="content flex-1 px-4 overflow-auto flex flex-col gap-4">
-      <div class="section-content" v-for="conf in settings">
+      <div class="section-content" v-for="conf in settings" :key="conf.id">
         <span :id="conf.id"></span>
         <component :is="conf.Component" />
       </div>
@@ -90,10 +94,19 @@ function clickMenu(conf: { id: string }) {
 }
 
 .menu-item {
-  --uno: px-2 text-gray-9 flex items-center;
+  --uno: px-2 text-gray-9 flex items-center py-2;
   cursor: pointer;
 
   text-decoration: none;
+
+  &:hover {
+    --uno: bg-light-7;
+  }
+
+  &.active {
+    --uno: bg-light-8;
+  }
+
 }
 
 .section-content {
