65bbcaf24a68686498e592fe686e9314622eacb0
Jerry Wang <x.jerry.wang@gmail.com>
2025-03-22 14:01:31 +0800

    fix: build script

--- scripts/build-helper.ts
+++ scripts/build-helper.ts
@@ -1,4 +1,5 @@
 import { exec } from '@0x-jerry/utils/node'
+import { execSync } from 'node:child_process'
 import { copyFile } from 'node:fs/promises'
 import path from 'node:path'
 
@@ -7,9 +8,10 @@ buildTextSelectionHelper()
 async function buildTextSelectionHelper() {
   const cwd = path.resolve('src-tauri')
 
+  const name = 'text-selection-helper'
   const binFileConfig = {
-    from: path.join(cwd, 'target/release/text-selection-helper'),
-    to: path.join(cwd, 'bin/text-selection-helper'),
+    from: path.join(cwd, `target/release/${name}`),
+    to: path.join(cwd, `bin/${name}-${await getSuffix()}`),
   }
 
   process.chdir(cwd)
@@ -21,4 +23,17 @@ async function buildTextSelectionHelper() {
   await copyFile(binFileConfig.from, binFileConfig.to)
 
   console.log('Done')
+}
+
+async function getSuffix() {
+  const extension = process.platform === 'win32' ? '.exe' : ''
+
+  const rustInfo = execSync('rustc -vV').toString('utf8')
+  const targetTriple = /host: (\S+)/g.exec(rustInfo)?.[1]
+
+  if (!targetTriple) {
+    throw new Error('Failed to determine platform target triple')
+  }
+
+  return `${targetTriple}${extension}`
 }
