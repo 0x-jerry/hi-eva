1c2dc1590825e18a75fd2857f778b71786c41a26
Jerry <x.jerry.wang@gmail.com>
2025-03-25 13:55:23 +0800

    feat: add auto update config

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -30,6 +30,7 @@ pub fn run() {
             commands::hide_toolbar_window,
         ])
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_updater::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
             log::info!("single instance");
