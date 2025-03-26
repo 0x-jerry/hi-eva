32f26205b17407ff18d5af52d491b88fbdfa462a
Jerry <x.jerry.wang@gmail.com>
2025-03-26 21:16:19 +0800

    perf: improve window position calculation

--- src-tauri/src/core/utils.rs
+++ src-tauri/src/core/utils.rs
@@ -1,18 +1,24 @@
-use tauri::{LogicalPosition, Manager, WebviewWindow};
+use tauri::{LogicalPosition, Manager, PhysicalPosition, WebviewWindow};
 
 use super::VerticalMoveDir;
 
 pub fn calc_window_position(
     win: &WebviewWindow,
     dir: Option<VerticalMoveDir>,
-) -> LogicalPosition<f64> {
+) -> PhysicalPosition<f64> {
     let offset_y = if dir.unwrap_or_default() == VerticalMoveDir::Up {
         -(16.0 + 36.0)
     } else {
         16.0
     };
+    let mouse_pos = win.cursor_position().unwrap();
 
-    let offset_pos = LogicalPosition::new(-20.0, offset_y);
+    let current_monitor_scale_factor = win
+        .app_handle()
+        .monitor_from_point(mouse_pos.x, mouse_pos.y)
+        .unwrap()
+        .unwrap()
+        .scale_factor();
 
     let app_scale_factor = win
         .app_handle()
@@ -21,12 +27,12 @@ pub fn calc_window_position(
         .unwrap()
         .scale_factor();
 
-    let mouse_pos = win
-        .cursor_position()
-        .unwrap()
-        .to_logical::<f64>(app_scale_factor);
+    let offset_pos =
+        LogicalPosition::new(-20.0, offset_y).to_physical::<f64>(current_monitor_scale_factor);
+
+    let mouse_pos = mouse_pos.to_logical::<f64>(app_scale_factor);
 
     let pos = LogicalPosition::new(mouse_pos.x + offset_pos.x, mouse_pos.y + offset_pos.y);
 
-    pos
+    pos.to_physical(app_scale_factor)
 }
