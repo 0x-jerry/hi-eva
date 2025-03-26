dcc333d289a556ef2c3035b449fcfcb97e6b91a7
Jerry Wang <x.jerry.wang@gmail.com>
2025-03-26 22:07:42 +0800

    fix: crash on macos

--- src-tauri/src/core/utils.rs
+++ src-tauri/src/core/utils.rs
@@ -11,14 +11,6 @@ pub fn calc_window_position(
     } else {
         16.0
     };
-    let mouse_pos = win.cursor_position().unwrap();
-
-    let current_monitor_scale_factor = win
-        .app_handle()
-        .monitor_from_point(mouse_pos.x, mouse_pos.y)
-        .unwrap()
-        .unwrap()
-        .scale_factor();
 
     let app_scale_factor = win
         .app_handle()
@@ -27,10 +19,12 @@ pub fn calc_window_position(
         .unwrap()
         .scale_factor();
 
-    let offset_pos =
-        LogicalPosition::new(-20.0, offset_y).to_physical::<f64>(current_monitor_scale_factor);
+    let mouse_pos = win
+        .cursor_position()
+        .unwrap()
+        .to_logical::<f64>(app_scale_factor);
 
-    let mouse_pos = mouse_pos.to_logical::<f64>(app_scale_factor);
+    let offset_pos = LogicalPosition::new(-20.0, offset_y);
 
     let pos = LogicalPosition::new(mouse_pos.x + offset_pos.x, mouse_pos.y + offset_pos.y);
 
