1bb2ba585343198ae54957b27e21a296cd25ee9f
Jerry <x.jerry.wang@gmail.com>
2025-04-17 14:26:02 +0800

    feat: remove draggable area and close icon

--- src/pages/toolbar.vue
+++ src/pages/toolbar.vue
@@ -1,8 +1,5 @@
 <script lang="ts" setup>
 import AutoResizeContainer from '../components/AutoResizeContainer.vue'
-import CloseWindow from '../components/CloseWindow.vue'
-import DraggableArea from '../components/DraggableArea.vue'
-import Icon from '../components/Icon.vue'
 import { type ToolbarPromptConfig, promptConfigs } from '../logic/config'
 import CarbonIcon from '../components/CarbonIcon.vue'
 import { commands } from '../logic/commands'
@@ -25,15 +22,11 @@ async function hideWindow() {
 <template>
   <AutoResizeContainer>
     <div class="toolbar bg-white flex">
-      <DraggableArea class="px-1 flex items-center cursor-move bg-gray-1">
-        <Icon class="i-carbon:draggable cursor-move" />
-      </DraggableArea>
       <div v-for="conf in promptConfigs" class="flex items-center px-2 hover:bg-gray-2 cursor-pointer"
         @click="openChatPage(conf)">
         <CarbonIcon v-if="conf.icon" :name="conf.icon" />
         <span v-else>{{ conf.name }}</span>
       </div>
-      <CloseWindow :close="hideWindow" class="py-2 px-1 flex items-center hover:bg-gray-2" />
     </div>
   </AutoResizeContainer>
 </template>
