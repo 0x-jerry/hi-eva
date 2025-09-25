5665b65561db95d7cff8042db14af8547e310f0c
Jerry <x.jerry.wang@gmail.com>
2025-09-25 18:23:47 +0800

    perf: upgrade tauri-nspanel

--- src-tauri/src/plugins/mac_win_ext.rs
+++ src-tauri/src/plugins/mac_win_ext.rs
@@ -1,17 +1,23 @@
-use tauri::{Manager, Result, Runtime, WebviewWindow};
+use tauri::{Manager, Result, WebviewWindow};
 
 use tauri_nspanel::{
-    cocoa::{
-        appkit::{NSMainMenuWindowLevel, NSView, NSWindow, NSWindowCollectionBehavior},
-        base::id,
-    },
-    objc::{msg_send, sel, sel_impl},
-    ManagerExt, WebviewWindowExt,
+    tauri_panel, CollectionBehavior, ManagerExt, PanelLevel, StyleMask, WebviewWindowExt,
 };
 
 use crate::core::AppState;
 
-pub trait MacWindowExt<R: Runtime> {
+tauri_panel! {
+    panel!(HoverActivatePanel {
+        config: {
+            can_become_main_window: false,
+            can_become_key_window: true,
+            becomes_key_only_if_needed: true,
+            is_floating_panel: true
+        }
+    })
+}
+
+pub trait MacWindowExt {
     fn ns_resign_focus(&self) -> Result<()>;
     fn ns_focus(&self) -> Result<()>;
     fn to_non_active_panel(&self) -> Result<()>;
@@ -19,25 +25,21 @@ pub trait MacWindowExt<R: Runtime> {
     fn set_radius(&self, radius: f64) -> Result<()>;
 }
 
-impl<R: Runtime> MacWindowExt<R> for WebviewWindow<R> {
+impl MacWindowExt for WebviewWindow {
     fn to_non_active_panel(&self) -> Result<()> {
-        let panel = self.to_panel()?;
+        let panel = self.to_panel::<HoverActivatePanel>()?;
 
-        // Set panel level
-        panel.set_level(NSMainMenuWindowLevel + 1);
-
-        // Allows the panel to display on the same space as the full screen window
-        panel.set_collection_behaviour(
-            NSWindowCollectionBehavior::NSWindowCollectionBehaviorCanJoinAllSpaces
-                | NSWindowCollectionBehavior::NSWindowCollectionBehaviorStationary
-                | NSWindowCollectionBehavior::NSWindowCollectionBehaviorFullScreenAuxiliary,
+        panel.set_level(PanelLevel::Floating.value());
+        panel.set_style_mask(StyleMask::empty().nonactivating_panel().into());
+        panel.set_collection_behavior(
+            CollectionBehavior::new()
+                .full_screen_auxiliary()
+                .can_join_all_spaces()
+                .into(),
         );
 
-        #[allow(non_upper_case_globals)]
-        const NSWindowStyleMaskNonActivatingPanel: i32 = 1 << 7;
-
-        // Ensures the panel cannot activate the App
-        panel.set_style_mask(NSWindowStyleMaskNonActivatingPanel);
+        panel.set_hides_on_deactivate(false);
+        panel.set_works_when_modal(true);
 
         Ok(())
     }
@@ -50,7 +52,7 @@ impl<R: Runtime> MacWindowExt<R> for WebviewWindow<R> {
         if !state.toolbar.focused {
             let panel = self.get_webview_panel(self.label()).unwrap();
             self.run_on_main_thread(move || {
-                panel.show();
+                panel.show_and_make_key();
             })
             .unwrap();
 
@@ -82,17 +84,20 @@ impl<R: Runtime> MacWindowExt<R> for WebviewWindow<R> {
 
     fn set_radius(&self, radius: f64) -> Result<()> {
         let win = self.ns_window()?;
-        let win = win as id;
 
         // Set window radius
         unsafe {
-            let view: id = win.contentView();
+            let win = win as *mut NSWindow;
+
+            let win = win.as_ref().unwrap();
+
+            let view = win.contentView().unwrap();
 
             view.wantsLayer();
 
-            let layer: id = view.layer();
+            let layer = view.layer().unwrap();
 
-            let _: () = msg_send![layer, setCornerRadius: radius];
+            let _: () = msg_send![&*layer, setCornerRadius: radius];
         }
         Ok(())
     }
