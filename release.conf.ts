d84bbc7a7dd522780049629f9480619eaacbdc01
Jerry <x.jerry.wang@gmail.com>
2025-11-19 00:33:25 +0800

    perf: upgrade deps

--- release.conf.ts
+++ release.conf.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from '@0x-jerry/x-release'
-import { readJson } from '@0x-jerry/utils/node'
 import { writeFile } from 'node:fs/promises'
+import { readJson } from '@0x-jerry/utils/node'
+import { defineConfig } from '@0x-jerry/x-release'
 
 export default defineConfig({
   publish: false,
   async beforeCommit(ctx) {
     const confPath = 'src-tauri/tauri.conf.json'
-    const conf = await readJson<Record<string,string>>(confPath) || {}
+    const conf = (await readJson<Record<string, string>>(confPath)) || {}
 
     conf.version = ctx.nextVersion.replace(/[a-z]+\./g, '')
 
--- src-tauri/src/sql.rs
+++ src-tauri/src/sql.rs
@@ -2,17 +2,20 @@ use tauri::{plugin::TauriPlugin, Runtime};
 use tauri_plugin_sql::{Migration, MigrationKind, PluginConfig};
 
 pub fn init_sql<T: Runtime>() -> TauriPlugin<T, Option<PluginConfig>> {
-    let migrations = vec![Migration {
-        version: 1,
-        description: "create_initial_tables",
-        sql: include_str!("sql/init_20240418.sql"),
-        kind: MigrationKind::Up,
-    }, {
-        version: 2,
-        description: "create history relative tables",
-        sql: include_str!("sql/init_20251014.sql"),
-        kind: MigrationKind::Up,
-    }];
+    let migrations = vec![
+        Migration {
+            version: 1,
+            description: "create_initial_tables",
+            sql: include_str!("sql/init_20240418.sql"),
+            kind: MigrationKind::Up,
+        },
+        Migration {
+            version: 2,
+            description: "create history relative tables",
+            sql: include_str!("sql/init_20251014.sql"),
+            kind: MigrationKind::Up,
+        },
+    ];
 
     tauri_plugin_sql::Builder::default()
         .add_migrations("sqlite:data.db", migrations)
