ee74126908993b35ffe250c0921d171be4c1043b
Jerry <x.jerry.wang@gmail.com>
2025-11-20 20:10:04 +0800

    chore: format code

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
+import Aura from '@primeuix/themes/aura'
+import PrimeVue from 'primevue/config'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
-
-import Aura from '@primeuix/themes/aura'
-import PrimeVue from 'primevue/config'
 
 import 'normalize.css'
 import 'uno.css'
