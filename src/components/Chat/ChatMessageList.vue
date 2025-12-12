6b448774a3a615d6156dd42fabef162e046286f2
Jerry <x.jerry.wang@gmail.com>
2025-12-12 21:54:48 +0800

    fix: auto scroll to end

--- src/components/Chat/ChatMessageList.vue
+++ src/components/Chat/ChatMessageList.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { MessagePlugin } from 'tdesign-vue-next'
-import { nextTick, ref, watch } from 'vue'
 import { useI18n } from '../../composables'
 import type { IChatHistoryMsgItem } from '../../database/chatHistoryMsg'
 import ChatMessage, { ChatMessageEmits } from './ChatMessage.vue'
@@ -14,21 +13,8 @@ export interface ChatMessageListEmits extends Omit<ChatMessageEmits, 'copy'> {}
 const props = defineProps<ChatMessageListProps>()
 
 const emit = defineEmits<ChatMessageListEmits>()
-const container = ref<HTMLElement | null>(null)
 
 const { t } = useI18n()
-
-watch(
-  () => props.messages.length,
-  async () => {
-    await nextTick()
-
-    container.value?.scrollTo({
-      top: container.value.scrollHeight,
-      behavior: 'smooth',
-    })
-  },
-)
 
 async function handleCopy(msg: IChatHistoryMsgItem) {
   const content = msg.content
@@ -38,7 +24,7 @@ async function handleCopy(msg: IChatHistoryMsgItem) {
 </script>
 
 <template>
-  <div class="message-list" ref="container">
+  <div class="message-list">
     <ChatMessage v-for="m in messages.toReversed()" :key="m.id" :message="m" @copy="handleCopy"
       @reset-to="emit('reset-to', $event)" @delete="emit('delete', $event)" @continue="emit('continue', $event)" />
   </div>
--- src/components/Chat/ChatRoot.vue
+++ src/components/Chat/ChatRoot.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { nextTick, useTemplateRef, watch } from 'vue'
 import type { IChatHistoryMsgItem } from '../../database/chatHistoryMsg'
 import ChatInput, { type ChatInputEmits } from './ChatInput.vue'
 import ChatMessageList from './ChatMessageList.vue'
@@ -15,9 +16,23 @@ export interface ChatRoomEmits extends ChatInputEmits {
   'continue-from-msg': [msg: IChatHistoryMsgItem]
 }
 
-defineProps<ChatRoomProps>()
+const props = defineProps<ChatRoomProps>()
 
 const emit = defineEmits<ChatRoomEmits>()
+
+const containerEl = useTemplateRef('containerEl')
+
+watch(
+  () => props.messages.length,
+  async () => {
+    await nextTick()
+
+    containerEl.value?.scrollTo({
+      top: containerEl.value.scrollHeight,
+      behavior: 'smooth',
+    })
+  },
+)
 
 function handleSend(content: string) {
   emit('send', content)
@@ -26,7 +41,7 @@ function handleSend(content: string) {
 
 <template>
   <div class="chat-root">
-    <div class="flex-1 h-0 overflow-auto p-3">
+    <div class="flex-1 h-0 overflow-auto p-3" ref="containerEl">
       <ChatMessageList :messages="messages" @delete="emit('delete-msg', $event)"
         @reset-to="emit('reset-to-msg', $event)" @continue="emit('continue-from-msg', $event)" />
     </div>
